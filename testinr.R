d5be266cf202fcdd0fc06ce83ac8d11f2979282a
alanwardnhiu <alan.ward1@hse.ie>
2023-04-13 16:27:07 +0100

    commit 3

--- testinr.R
+++ testinr.R
@@ -1,3 +1,11 @@
 #Git in r test. AW.
 
 #Additional coding added for second commit
+
+create_github_token(
+  scopes = c("repo", "user", "gist", "workflow"),
+  description = "DESCRIBE THE TOKEN'S USE CASE",
+  host = NULL
+)
+
+gh_token_help(host = NULL)
